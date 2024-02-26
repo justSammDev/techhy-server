a07a1a8ad24bec2c1cd91f71ca744743d0d3c235
Samridda Devkota <samriddadevkota@gmial.com>
2024-02-26 19:47:17 +0545

    Ready for deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,3 @@
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://www.techhynotes.onrender.com",
-];
+const allowedOrigins = ["https://techhynotes.onrender.com"];
 
 module.exports = allowedOrigins;
